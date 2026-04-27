c175a14df9817021af1531a0b5b83270126ad7cd
ayushkumar21161 <ayush@homeasy.io>
2026-04-27 17:51:27 +0530

    Updated project

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -128,7 +128,8 @@ export default function Resources() {
               // src="https://lh3.googleusercontent.com/aida-public/AB6AXuAbnDFLP43Fn141nKcxJbanMu6mYas6vX-2s8-gNzG-74JMQktYuYVbszGHGkJ7_1RgoVhuouFPrMG90d4m7TaYpnomQpLT_906UyIEeaq094pU2igxg-LpIQNpjoO_uMRNdYdiEDYBVdzi8FTaX4HU_fJ2wIZ5OCRzW6mFCRVwpbRDNxiedY-UphCaqzCnRQ_TohLsAt3dUQWZBLINzszW5FVQ0q9WGszV1ehZLN-bbdrijV9rpW3gWBNk7kmAb5tczz59j4-kVpQ"
               alt="Cinematic wide shot founders"
             />
-            <div className="absolute inset-0 bg-gradient-to-t from-background-lowest via-transparent to-transparent" />
+            {/* <div className="absolute inset-0 bg-gradient-to-t from-background-lowest via-transparent to-transparent" /> */}
+            <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent" />
           </div>
           
           <div className="relative z-10 max-w-5xl">
